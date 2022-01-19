d2a297d1a0f7ddcab7cfe88a6155e8844e899251
Kike <kike.delacalle.montilla@gmail.com>
2022-01-19 19:33:45 +0100

    sorted mtl

--- workspace/RESTServer/src/Main.java
+++ workspace/RESTServer/src/Main.java
@@ -15,24 +15,13 @@ public class Main {
 		
 			@Override
 			public Object handle(Request request, Response response){
-				String author = request.queryParams("author");
-				String title = request.queryParams("title");
-				Book book = new Book(author, title);
 				
-				response.status(201);
-				return "id";
 			}
 		});
 		get(new Route("/books") {
 		
 			@Override
 			public Object handle(Request request, Response response){
-				
-				String ids = "";
-				for(String id : books.keySet())
-					ids += id + " ";
-				response.status(200);
-				return "id";
 				
 			}
 		});
@@ -41,15 +30,6 @@ public class Main {
 			@Override
 			public Object handle(Request request, Response response){
 				
-				Book book = books.get(request.params(":id"));
-				if (book != null) {
-					response.status(200);
-					return "id";
-				} else {
-					response.status(404);
-					return "error";
-				}
-				
 			}
 		});
 		put(new Route("/books/:id") {
@@ -57,44 +37,15 @@ public class Main {
 			@Override
 			public Object handle(Request request, Response response){
 				
-				String id = request.params(":id");
-				Book book = books.get(id);
-				if (book != null) {
-				
-					String newAuthor = request.queryParams("author");
-					if (newAuthor != null)
-						book.setAuthor(newAuthor);
-					String newTitle = request.queryParams("title");
-					if (newTitle != null)
-						book.setTitle(newTitle);
-				
-					response.status(201);
-					return "id";	
-				
-				} else {
-					response.status(404);
-					return "error";
-				}
-				
 			}
 		});
 		delete(new Route("/books/:id") {
 		
 			@Override
 			public Object handle(Request request, Response response){
-				String id = request.params(":id");
-				Book book = books.remove(id);
-				if (book != null) {
-					response.status(200);
-					return "id";
-				} else {
-					response.status(404);
-					return "error";
-				}
 				
 			}
 		});
-
 	}
 }
 
